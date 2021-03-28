ae601953cffbba6f68cbfc3e27e5442925da53f4
ubuntovini <viniciusoliveiras12@hotmail.com.br>
2021-03-28 14:00:57 -0300

    feat: Critical region 2 resolved

--- trem.cpp
+++ trem.cpp
@@ -2,6 +2,7 @@
 #include <QtCore>
 #include <QMutex>
 #include <iostream>
+#include <QSemaphore>
 
 #define MAX_SPEED 120
 
@@ -12,6 +13,10 @@ QMutex *mutex4 = new QMutex();
 QMutex *mutex5 = new QMutex();
 QMutex *mutex6 = new QMutex();
 QMutex *mutex7 = new QMutex();
+
+QSemaphore *semaphoreRegion256 = new QSemaphore(2);
+QSemaphore *semaphoreRegion457 = new QSemaphore(2);
+QSemaphore *semaphoreRegion134 = new QSemaphore(2);
 
 using namespace std;
 
@@ -33,7 +38,11 @@ void Trem::run()
         {
         case 1: //Trem 1
             if (y == 70 && x < 310)
-                if (x == 290)
+                if (x == 280){
+                    semaphoreRegion134->acquire();
+                    x += 10;
+                }
+                else if (x == 290)
                 {
                     mutex1->lock();
                     x += 10;
@@ -61,6 +70,7 @@ void Trem::run()
                 else if (x == 160)
                 {
                     mutex3->unlock();
+                    semaphoreRegion134->release();
                     x -= 10;
                 }
                 else
@@ -78,6 +88,10 @@ void Trem::run()
                     mutex1->unlock();
                     x += 10;
                 }
+                else if (x == 550){
+                    semaphoreRegion256->acquire();
+                    x += 10;
+                }
                 else if (x == 560)
                 {
                     mutex2->lock();
@@ -90,6 +104,9 @@ void Trem::run()
             else if (x == 580 && y < 210){
                 if(y == 190){
                     mutex5->lock();
+                    mutex4->unlock();
+                    semaphoreRegion256->release();
+//                    semaphoreRegion457->release();
                     y += 10;
                 }
                 else
@@ -111,13 +128,32 @@ void Trem::run()
                 else if (x == 430){
                     mutex5->unlock();
                     x-=10;
+                }else if (x == 480){
+                     semaphoreRegion134->acquire();
+                     x -= 10;
+                }
+                else if (x == 470){
+                    mutex4->lock();
+
+                    x -= 10;
                 }
                 else
                 {
                     x -= 10;
                 }
-            else
-                y -= 10;
+            else {
+                if(x == 310 && y == 190){
+                    semaphoreRegion134->release();
+                     y -= 10;
+                }
+                else if (y == 180){
+//                    semaphoreRegion457->acquire();
+                    y -= 10;
+                }
+                else {
+                    y -= 10;
+                }
+            }
             emit updateGUI(ID, x, y); //Emite um sinal
             break;
         case 3: //Trem 3
@@ -135,13 +171,19 @@ void Trem::run()
             }
             else if (x == 850 && y < 210)
             {
-                y += 10;
+                if(y == 190){
+                    semaphoreRegion256->acquire();
+                    y += 10;
+                }else{
+                    y += 10;
+                }
             }
             else if (x > 580 && y == 210)
             {
                 if (x == 600)
                 {
                     mutex2->lock();
+                    semaphoreRegion256->release();
                     x -= 10;
                 }
                 else if (x == 740)
@@ -173,38 +215,52 @@ void Trem::run()
                 if (x == 430)
                 {
                     mutex7->lock();
+                    semaphoreRegion134->release();
+//                    semaphoreRegion457->release();
                     x += 10;
                 }
                 else if (x == 330)
                 {
                     mutex3->unlock();
                     x += 10;
+                }else if (x == 290){
+                    mutex4->lock();
+//                    semaphoreRegion457->acquire();
+                    x += 10;
                 }
                 else
                 {
                     x += 10;
                 }
-            else if (x == 450 && y < 330)
-                y += 10;
+            else if (x == 450 && y < 330){
+                   y += 10;
+             }
             else if (x > 180 && y == 330)
                 if (x == 430)
                 {
                     mutex7->unlock();
                     x -= 10;
                 }
+                else if (x == 200){
+                    semaphoreRegion134->acquire();
+                    x -= 10;
+                }
                 else
                 {
                     x -= 10;
                 }
-            else if (x == 180 && y == 230)
-            {
-                mutex3->lock();
-                y -= 10;
-            }
-            else
-            {
-                y -= 10;
-            }
+            else {
+                if (x == 180 && y == 230)
+                {
+                    mutex3->lock();
+                    mutex4->unlock();
+                    y -= 10;
+                 }
+                else
+                {
+                    y -= 10;
+                }
+             }
             emit updateGUI(ID, x, y); //Emite um sinal
             break;
         case 5: //Trem 5
@@ -218,10 +274,13 @@ void Trem::run()
                 else if (x == 560)
                 {
                     mutex6->lock();
+                    semaphoreRegion256->acquire();
                     x += 10;
                 }
                 else if (x == 600){
                     mutex5->unlock();
+                    semaphoreRegion256->release();
+//                    semaphoreRegion457->release();
                     x+= 10;
                 }
                 else
@@ -240,7 +299,11 @@ void Trem::run()
                     y += 10;
                 }
             else if (x > 450 && y == 330)
-                if (x == 470)
+                if (x == 490){
+//                    semaphoreRegion457->acquire();
+                     x -= 10;
+                }
+                else if (x == 470)
                 {
                     mutex7->lock();
                     x -= 10;
